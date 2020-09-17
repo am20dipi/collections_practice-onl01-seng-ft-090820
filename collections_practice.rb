2d70ab09898c58297cc2c15daa75b2c04d7ff007
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 18:14:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,9 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  return array.sort.reverse {|word| word.length}
+  array.sort do |word|
+    word.length.reverse
+  end
 end
 
 def swap_elements(array)
