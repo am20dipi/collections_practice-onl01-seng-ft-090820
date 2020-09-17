6a774d85996c591bac4158e1cefb27224f8801c6
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 18:34:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,10 +7,10 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort_by{|word| word.split.count}
+  #array.sort_by{|word| word.split.count}
+  array.sort{|b,a| b.length <=> a.length}
 end
 
-array.reverse
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
