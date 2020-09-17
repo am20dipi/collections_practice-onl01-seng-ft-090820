65d47874ebbb5003454f881c171c170761cc1fe8
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 18:21:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,3 +39,19 @@ def find_winners(hash)
   end
   winners
 end
+
+def sum_array(array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
+end
