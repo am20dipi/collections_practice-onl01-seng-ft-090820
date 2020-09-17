70afb410a46ca63b2aab13e045be23f05a5b4950
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 18:02:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  return array.sort {|word| word.length}
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def find_a(array)
+end
